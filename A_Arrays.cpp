6330f00871a2fde5007e31c7116a921ce4f183de
Arpita Das <arpita1477@gmail.com>
2026-04-21 21:02:53 +0600

    Add files via upload

---
+++ A_Arrays.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+    int nA,nB,k,m;
+    cin>>nA>>nB>>k>>m;
+    vector<int> A(nA),B(nB);
+
+    for(int i=0;i<nA;i++){
+        cin>>A[i];
+    }
+    for(int i=0;i<nB;i++){
+        cin>>B[i];
+    }
+    if(A[k-1]<B[nB-m]){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
