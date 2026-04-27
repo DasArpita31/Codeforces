e3e125f7482b78efaa60a702beb50797461876c4
Arpita Das <arpita1477@gmail.com>
2026-04-27 23:44:50 +0600

    Add files via upload

---
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+        int p;
+        cin>>p;
+        int arr[p];
+    for(int i=0;i<=n;i++){
+        cin>>arr[i];
+    }
+    int q;
+    cin>>q;
+    int arr[q];
+    for(int i=0;i<q;i++){
+        cin>>arr[i];
+    }
+    
+}
