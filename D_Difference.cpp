ede02b79592bb7b172066a528f5a4976b04df71a
Arpita Das <arpita1477@gmail.com>
2026-05-26 23:38:33 +0600

    Add files via upload

---
+++ D_Difference.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long A,B,C,D;
+    cin>>A>>B>>C>>D;
+    long long sum=0;
+    sum=(A*B)-(C*D);
+    cout<<"Difference = ";
+    cout<<sum<<endl;
+}
