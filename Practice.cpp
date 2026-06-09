48305f4aadbfbf72c5659a45a58edd0cc062204e
Arpita Das <arpita1477@gmail.com>
2026-06-09 20:29:43 +0600

    Add files via upload

---
+++ Practice.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"I am even."<<endl;
+    }
+    else
+        cout<<"I am odd"<<endl;
+}
